17e5126a3c770191ba55079298edf85c9c5abd8c
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-10 20:39:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-        name='prog',
+        name='cmeson',
         version='0.1.0',
         author='Michael Brockus',
         license='Apache 2',
-        url='https://github.com/michaelbrockus/python_pypi_program',
+        url='https://github.com/michaelbrockus/cmeson',
         packages=[
             'code'
         ],
         python_requires='>=3.8',
         entry_points={
             'console_scripts': [
-                'prog=code.main:main_prog',
+                'cmeson=code.main:main_prog',
             ],
         }
     )
